7ca14721bf9e8adaa2b4ac601bfe658f704ac9a1
Mokut001 <mokutoton01@gmail.com>
2026-02-16 04:10:40 +0100

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: { extend: { colors: { cardano: "#0033ad" } } },
-  plugins: [],
-}
